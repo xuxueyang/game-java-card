0591916f08762f7cf0966475219068a626e9817e
xuxy <xuxy@fengyuntec.com>
2019-09-12 15:35:32 +0800

    update

--- netty/src/main/java/config/RabbitMQConfiguration.java
+++ netty/src/main/java/config/RabbitMQConfiguration.java
@@ -2,6 +2,10 @@ package config;
 
 import core.rpc.ChatRPCConstant;
 import core.rpc.RoomRPCConstant;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import rabbitmq.mq.consumer.RabbitMQConsumer;
@@ -12,7 +16,52 @@ import java.util.concurrent.TimeoutException;
 
 @Configuration
 public class RabbitMQConfiguration {
+    @Value("${rabbitmq.mq.host}")
+    private String host;
+
+    @Value("${rabbitmq.mq.username}")
+    private String username;
+
+    @Value("${rabbitmq.mq.password}")
+    private String password;
+    @Value("${rabbitmq.mq.port}")
+    private String port;
+
+//    @Value("${rabbitmq.mq.vhost}")
+//    private String vhost;
+
+
+
+    /** * 因为使用了自定义的ConnectionFactory,所以需要定义RabbitAdmin * */
+//    @Bean(value = "nettyRabbitAdmin")
+//    public RabbitAdmin pmsRabbitAdmin(){
+//        RabbitAdmin rabbitAdmin=new RabbitAdmin(pmsMqConnectionFactory());
+//        return rabbitAdmin;
+//    }
+//    @Bean(value = "nettyConnectionFactory")
+//    public ConnectionFactory pmsMqConnectionFactory(){
+//        CachingConnectionFactory connectionFactory=new CachingConnectionFactory();
+//        connectionFactory.setAddresses(host);
+//        connectionFactory.setVirtualHost(host + ":" + port);
+//        connectionFactory.setUsername(username);
+//        connectionFactory.setPassword(password);
+//        return connectionFactory;
+//    }
+
 //    该方法名默认就是Bean名
+    @Bean(name = RoomRPCConstant.MQ_NAME_PRODUCER)
+    public RabbitMQConsumer RoomRabbitMQConsumer(){
+        RabbitMQConsumer Consumer = null;
+        try {
+            Consumer = new RabbitMQConsumer(RoomRPCConstant.MQ_NAME_PRODUCER);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+        return Consumer;
+    }
+
     @Bean(name = RoomRPCConstant.MQ_NAME_CONSUMER)
     public RabbitMQProducer RoomRabbitMQProducer (){
         RabbitMQProducer Producer = null;
@@ -25,11 +74,12 @@ public class RabbitMQConfiguration {
         }
         return Producer;
     }
-    @Bean(name = RoomRPCConstant.MQ_NAME_PRODUCER)
-    public RabbitMQConsumer RoomRabbitMQConsumer(){
+
+    @Bean(name = ChatRPCConstant.MQ_NAME_PRODUCER)
+    public RabbitMQConsumer ChatRabbitMQConsumer(){
         RabbitMQConsumer Consumer = null;
         try {
-            Consumer = new RabbitMQConsumer(RoomRPCConstant.MQ_NAME_PRODUCER);
+            Consumer = new RabbitMQConsumer(ChatRPCConstant.MQ_NAME_PRODUCER);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (TimeoutException e) {
@@ -50,16 +100,5 @@ public class RabbitMQConfiguration {
         }
         return Producer;
     }
-    @Bean(name = ChatRPCConstant.MQ_NAME_PRODUCER)
-    public RabbitMQConsumer ChatRabbitMQConsumer(){
-        RabbitMQConsumer Consumer = null;
-        try {
-            Consumer = new RabbitMQConsumer(ChatRPCConstant.MQ_NAME_PRODUCER);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-        }
-        return Consumer;
-    }
+
 }
